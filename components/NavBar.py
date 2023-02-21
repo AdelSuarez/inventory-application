dce75aba3295ee8d716634a488e9d872e3ba0fb0
adel suarez <adel90suarez@gmail.com>
2023-02-20 21:59:20 -0400

    code correction

--- components/NavBar.py
+++ components/NavBar.py
@@ -8,5 +8,5 @@ class Navbar(customtkinter.CTkFrame):
         self.configure( fg_color=setting.BACKGROUND, corner_radius=0)
         self.grid(row=0, column=0, sticky=customtkinter.NSEW, columnspan=2)
 
-        self.title = customtkinter.CTkLabel(master=self, text="SISTEMA DE INVENTARIO", font=customtkinter.CTkFont(family='Oswald bold', size=18))
+        self.title = customtkinter.CTkLabel(master=self, text="SISTEMA DE INVENTARIO", font=customtkinter.CTkFont(family='Oswald', size=18, weight="bold"))
         self.title.pack(side=customtkinter.LEFT, padx=20)
