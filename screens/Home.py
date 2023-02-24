a729cd7a1560daa690887c8e871a6826134884ec
adel suarez <adel90suarez@gmail.com>
2023-02-23 20:30:49 -0400

    The client visualization frame and improvements in the code were implemented

--- screens/Home.py
+++ screens/Home.py
@@ -10,47 +10,132 @@ class HomeScreen(customtkinter.CTkFrame):
         self._controller_frame()
         self._table()
 
-
     def _controller_frame(self):
         self._controller = customtkinter.CTkFrame(master=self, corner_radius=0)
         self._controller.grid(row=0, column=0, sticky=customtkinter.NSEW)
 
-        self.title_label = customtkinter.CTkLabel(self._controller, text="Crear Cliente", font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.title_label.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 10), sticky=customtkinter.W)
-
-        self.date = customtkinter.CTkLabel(self._controller, text="Datos:", font=customtkinter.CTkFont( weight="bold"))
-        self.date.grid(row=1, column=0, padx=30, sticky=customtkinter.W)
-
-        self.name_entry = customtkinter.CTkEntry(self._controller, placeholder_text="Nombre del cliente", height=40)
-        self.name_entry.focus()
-        self.name_entry.grid(row=2, column=0,columnspan=2, padx=20, sticky=customtkinter.NSEW)
-
-        self.dni_entry = customtkinter.CTkEntry(self._controller, placeholder_text="DNI", height=40)
-        self.dni_entry.grid(row=3, column=0, pady=(10,0), padx=(20, 10), sticky=customtkinter.NSEW)
-
-        self.tlf_entry = customtkinter.CTkEntry(self._controller, placeholder_text="Número telefonico", height=40)
-        self.tlf_entry.grid(row=3, column=1, pady=(10,0), padx=(0, 20), sticky=customtkinter.NSEW)
-
-        self.locatiom = customtkinter.CTkOptionMenu(self._controller, 
-                                                values=["option 1", "option 2"],
-                                                width=250)
-        self.locatiom.grid(row=4, column=1,pady=10,padx=(0,20), sticky=customtkinter.W)
-        self.locatiom.set("option 2")
-
-        self.btn_add_location = customtkinter.CTkButton(self._controller, text="Agregar ubicación", command=self._add_location)
-        self.btn_add_location.grid(row=4, column=0,pady=10, padx=(20,10), sticky=customtkinter.E)
-
-        self.megas_entry = customtkinter.CTkEntry(self._controller, placeholder_text="Megas", height=40)
-        self.megas_entry.grid(row=5, column=0, padx=(20, 10), sticky=customtkinter.NSEW)
-
-        self.ip_entry = customtkinter.CTkEntry(self._controller, placeholder_text="IP", height=40)
-        self.ip_entry.grid(row=5, column=1, padx=(0, 20), sticky=customtkinter.NSEW)
-
-        self.btn_add_location = customtkinter.CTkButton(self._controller, text="Crear cliente", command=self._add_client)
-        self.btn_add_location.grid(row=6, column=0, columnspan=2, padx=20, pady=10, sticky=customtkinter.NSEW)
+        self.tabview = customtkinter.CTkTabview(self._controller)
+        self.tabview.grid(row=0, column=0, padx=10, columnspan=2, sticky=customtkinter.NSEW)
+        self.tabview.add("Datos de cliente")
+        self.tabview.add("Crear cliente")
+        self.tabview.add("Configuraciones de Ubicacion")
+        self.tabview.tab("Datos de cliente").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.tab("Crear cliente").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Configuraciones de Ubicacion").grid_columnconfigure(0, weight=1)
+        self._view_client(self.tabview.tab('Datos de cliente'))
+        self._create_client(self.tabview.tab('Crear cliente'))
 
         self._controller.columnconfigure(0, weight=1)
         self._controller.columnconfigure(1, weight=1)
+
+    def _view_client(self, wind):
+        self._view_client_frame = customtkinter.CTkFrame(wind)
+        self._view_client_frame.configure(fg_color='transparent')
+        self._view_client_frame.pack(side = tk.TOP,
+            fill = tk.BOTH, 
+            expand = True)
+
+        self._name_title = customtkinter.CTkLabel(self._view_client_frame, text='Nombre:', font=customtkinter.CTkFont( weight="bold"))
+        self._name_title.grid(row=0, column=0, padx=(0, 10), pady=10)
+
+        self._name_entry_view = customtkinter.CTkEntry(self._view_client_frame, border_width=0, state='disabled', height=40)
+        self._name_entry_view.grid(row=0, column=1, pady=10, sticky=customtkinter.NSEW)
+
+        self._dni_tlf_frame = customtkinter.CTkFrame(self._view_client_frame)
+        self._dni_tlf_frame.configure(fg_color='transparent')
+        self._dni_tlf_frame.grid(row=1, column=0, columnspan=2, sticky=customtkinter.NSEW)
+
+        self._dni_title = customtkinter.CTkLabel(self._dni_tlf_frame, text='DNI:', font=customtkinter.CTkFont( weight="bold"))
+        self._dni_title.grid(row=0, column=0, padx=(0,10))
+
+        self._dni_entry_view = customtkinter.CTkEntry(self._dni_tlf_frame, border_width=0, state='disabled', height=40)
+        self._dni_entry_view.grid(row=0, column=1, padx=(0,5), sticky=customtkinter.NSEW)
+
+        self._tlf_title = customtkinter.CTkLabel(self._dni_tlf_frame, text='TLF:', font=customtkinter.CTkFont( weight="bold"))
+        self._tlf_title.grid(row=0, column=2, padx=(0,10))
+
+        self._tlf_entry_view = customtkinter.CTkEntry(self._dni_tlf_frame, border_width=0, state='disabled', height=40)
+        self._tlf_entry_view.grid(row=0, column=3, sticky=customtkinter.NSEW)
+
+        self._location_view = customtkinter.CTkOptionMenu(self._view_client_frame, 
+                                                values=["vacio"],
+                                                width=200)
+        self._location_view.grid(row=4, column=0,pady=10, columnspan=2, sticky=customtkinter.NSEW)
+        self._location_view.set("vacio")
+
+        self._mg_ip_frame = customtkinter.CTkFrame(self._view_client_frame)
+        self._mg_ip_frame.configure(fg_color='transparent')
+        self._mg_ip_frame.grid(row=5, column=0, columnspan=2, sticky=customtkinter.NSEW)
+
+        self._mg_title = customtkinter.CTkLabel(self._mg_ip_frame, text='MG:', font=customtkinter.CTkFont( weight="bold"))
+        self._mg_title.grid(row=0, column=0, padx=(0,10))
+
+        self._mg_entry_view = customtkinter.CTkEntry(self._mg_ip_frame, border_width=0, state='disabled', height=40)
+        self._mg_entry_view.grid(row=0, column=1, padx=(0,5), sticky=customtkinter.NSEW)
+
+        self._ip_title = customtkinter.CTkLabel(self._mg_ip_frame, text='IP:', font=customtkinter.CTkFont( weight="bold"))
+        self._ip_title.grid(row=0, column=2, padx=(0,10))
+
+        self._tlf_entry_view = customtkinter.CTkEntry(self._mg_ip_frame, border_width=0, state='disabled', height=40)
+        self._tlf_entry_view.grid(row=0, column=3, sticky=customtkinter.NSEW)
+
+
+        self._btn_frame = customtkinter.CTkFrame(self._view_client_frame)
+        self._btn_frame.configure(fg_color='transparent')
+        self._btn_frame.grid(row=6, column=0, pady=10, columnspan=2, sticky=customtkinter.NSEW)
+
+        self._btn_delete = customtkinter.CTkButton(self._btn_frame, text="Borrar", command=self._add_client)
+        self._btn_delete.grid(row=0, column=0, padx=(0,10), sticky=customtkinter.NSEW)
+        self._btn_edit = customtkinter.CTkButton(self._btn_frame, text="Editar", command=self._add_client)
+        self._btn_edit.grid(row=0, column=1, sticky=customtkinter.NSEW)
+
+        self._btn_frame.columnconfigure(0, weight=1)
+        self._btn_frame.columnconfigure(1, weight=1)
+        self._mg_ip_frame.columnconfigure(1, weight=1)
+        self._mg_ip_frame.columnconfigure(3, weight=2)
+        self._dni_tlf_frame.columnconfigure(1, weight=1)
+        self._dni_tlf_frame.columnconfigure(3, weight=2)
+        self._view_client_frame.columnconfigure(1, weight=1)
+
+    def _create_client(self, wind):
+        self._create_client_frame = customtkinter.CTkFrame(wind)
+        self._create_client_frame.configure(fg_color='transparent')
+        self._create_client_frame.pack(side = tk.TOP,
+            fill = tk.BOTH, 
+            expand = True)
+
+        self._date = customtkinter.CTkLabel(self._create_client_frame, text="Datos:", font=customtkinter.CTkFont( weight="bold"))
+        self._date.grid(row=1, column=0, padx=10, sticky=customtkinter.W)
+
+        self._name_entry = customtkinter.CTkEntry(self._create_client_frame, placeholder_text="Nombre del cliente", height=40)
+        self._name_entry.grid(row=2, column=0,columnspan=2, sticky=customtkinter.NSEW)
+
+        self._dni_entry = customtkinter.CTkEntry(self._create_client_frame, placeholder_text="DNI", height=40)
+        self._dni_entry.grid(row=3, column=0, pady=(10,0), padx=(0, 10), sticky=customtkinter.NSEW)
+
+        self._tlf_entry = customtkinter.CTkEntry(self._create_client_frame, placeholder_text="Número telefonico", height=40)
+        self._tlf_entry.grid(row=3, column=1, pady=(10,0), sticky=customtkinter.NSEW)
+
+        self._location = customtkinter.CTkOptionMenu(self._create_client_frame, 
+                                                values=["vacio"],
+                                                width=200)
+        self._location.grid(row=4, column=1,pady=10, sticky=customtkinter.W)
+        self._location.set("vacio")
+
+        self._btn_add_location = customtkinter.CTkButton(self._create_client_frame, text="Agregar ubicación", command=self._add_location)
+        self._btn_add_location.grid(row=4, column=0,pady=10, padx=(0,10), sticky=customtkinter.E)
+
+        self._megas_entry = customtkinter.CTkEntry(self._create_client_frame, placeholder_text="Megas", height=40)
+        self._megas_entry.grid(row=5, column=0, padx=(0, 10), sticky=customtkinter.NSEW)
+
+        self._ip_entry = customtkinter.CTkEntry(self._create_client_frame, placeholder_text="IP", height=40)
+        self._ip_entry.grid(row=5, column=1, sticky=customtkinter.NSEW)
+
+        self._btn_add_location = customtkinter.CTkButton(self._create_client_frame, text="Crear cliente", command=self._add_client)
+        self._btn_add_location.grid(row=6, column=0, columnspan=2, pady=10, sticky=customtkinter.NSEW)
+
+        self._create_client_frame.columnconfigure(0, weight=1)
+        self._create_client_frame.columnconfigure(1, weight=1)
 
 
     def _add_client(self):
