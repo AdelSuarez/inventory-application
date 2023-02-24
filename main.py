2e40138bdf464ba7040a3bab6e2c9eb95258f89e
adel suarez <adel90suarez@gmail.com>
2023-02-23 23:15:32 -0400

    implementation of the query that creates the customer table

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from Manager import Manager
+from database.DataBase import DataBase
+
+query = '''CREATE TABLE IF NOT EXISTS clients (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NAME TEXT NOT NULL,
+        DNI INTEGER NOT NULL UNIQUE,
+        TLF INTEGER,
+        LOCATION TEXT NOT NULL,
+        MG INTEGER NOT NULL,
+        IP INTEGER NOT NULL UNIQUE,
+        PAY TEXT
+)'''
+
 
 if __name__ == '__main__':
+    print('Run app')
+    DataBase()._connect_db(query)
     app = Manager()
     app.mainloop()
