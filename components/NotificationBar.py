c5c8bf32d02fb4d4298f57717af6eb144b6b6639
adel suarez <adel90suarez@gmail.com>
2023-02-23 20:28:00 -0400

    display improvements

--- components/NotificationBar.py
+++ components/NotificationBar.py
@@ -4,13 +4,26 @@ import settings.settings as setting
 class NotificationBar(customtkinter.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.grid(row=2, column=0, sticky=customtkinter.NSEW, columnspan=2)
-        self.configure( fg_color=setting.BACKGROUND, corner_radius=0)
+        self.grid(row=2, column=0, columnspan=2,  sticky=customtkinter.NSEW)
+        self.columnconfigure(0, weight=1)
+        # self.columnconfigure(1, weight=1)
+
+        self.configure(corner_radius=0)
         self._create_widgets()
 
     def _create_widgets(self):
 
-        self._user = customtkinter.CTkLabel(master=self, text='Developer')
-        self._user.grid(row=0, column=0, padx=20)
-        # self._version =customtkinter.CTkLabel(master=self, text='V0.0.1')
-        # self._user.grid(row=1, column=0, padx=20)
+        self._bar_frame = customtkinter.CTkFrame(self)
+        self._bar_frame.configure( fg_color=setting.BACKGROUND, corner_radius=0)
+        self._bar_frame.grid(row=0, column=0, sticky=customtkinter.NSEW)
+
+        self._user = customtkinter.CTkLabel(master=self._bar_frame, text='Developer')
+        self._user.grid(row=0, column=0, padx=20, sticky=customtkinter.W)
+        self._version =customtkinter.CTkLabel(master=self._bar_frame, text='v0.0.1')
+        self._version.grid(row=0, column=1, padx=20, sticky=customtkinter.E)
+
+        self._bar_frame.columnconfigure(0, weight=1)
+        self._bar_frame.columnconfigure(1, weight=1)
+
+
+        
