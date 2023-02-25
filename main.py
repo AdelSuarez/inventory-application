243575dad9183d35e0f984acf7ee7e852c36fe7d
adel suarez <adel90suarez@gmail.com>
2023-02-25 19:20:26 -0400

    folders

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Manager import Manager
 from database.DataBase import DataBase
 
-query = '''CREATE TABLE IF NOT EXISTS clients (
+query_client = '''CREATE TABLE IF NOT EXISTS clients (
         ID INTEGER PRIMARY KEY AUTOINCREMENT,
         NAME TEXT NOT NULL,
         DNI INTEGER NOT NULL UNIQUE,
@@ -12,9 +12,16 @@ query = '''CREATE TABLE IF NOT EXISTS clients (
         PAY TEXT
 )'''
 
+query_location = ''' CREATE TABLE IF NOT EXISTS location (
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                LOCATION TEXT NOT NULL
+        )
+'''
+
 
 if __name__ == '__main__':
     print('Run app')
-    DataBase()._connect_db(query)
+    DataBase()._connect_db(query_client)
+    DataBase()._connect_db(query_location)
     app = Manager()
     app.mainloop()
