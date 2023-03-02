4141f6526be7c240ebaae76d4d7a4a8271bd0344
adel suarez <adel90suarez@gmail.com>
2023-03-02 18:58:51 -0400

    documentation

--- Manager.py
+++ Manager.py
@@ -8,25 +8,26 @@ class Manager(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.container = customtkinter.CTkFrame(self)
-        self._configure_win()
+        self._configure_window()
         self._configure_self()
         self.container.pack(expand=True, fill=customtkinter.BOTH)
-        self._win()
+        self._window()
 
-    def _win(self):
+    def _window(self):
+        # main window widget structure
         self.navbar = Navbar(master=self.container)
         self.Home = HomeScreen(master=self.container)
         self.notification_bar = NotificationBar(master=self.container )
 
-    def _configure_win(self):
-        """Main window setting"""
+    def _configure_window(self):
+        # Main window setting
         self.geometry('1200x700')
         self.minsize(width=1200, height=700)
         self.title('Control de clientes')
 
 
     def _configure_self(self):
-        """Configuration of the upper menu, so that it is expandible"""
+        # Configuration of the upper menu, so that it is expandible
         self.container.columnconfigure(0, weight=1)
         self.container.rowconfigure(0, minsize=70)
         self.container.rowconfigure(1, weight=1,)
