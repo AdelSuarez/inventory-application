b30b3136688b94ae54b3ef9401a457345b35e8b3
adel suarez <adel90suarez@gmail.com>
2023-03-04 21:33:14 -0400

    Logout button implementation

--- components/NavBar.py
+++ components/NavBar.py
@@ -3,14 +3,16 @@ import customtkinter
 import threading
 import time
 # import datetime
-
+import os
+from PIL import Image
 import settings.settings as setting
 
 
 class Navbar(customtkinter.CTkFrame):
-    def __init__(self, master, *args, **kwargs):
-        customtkinter.CTkFrame.__init__( self, master, *args, **kwargs)
-        self._master= master 
+    def __init__(self, parent, manager):
+        customtkinter.CTkFrame.__init__( self, parent)
+        self.manager= manager 
+        self._icon_sign_off = customtkinter.CTkImage(Image.open(os.path.join("img/", "sign_off.png")), size=(28, 28))
         # Settings Frame
         self.configure( fg_color=setting.BACKGROUND, corner_radius=0)
         self.grid(row=0, column=0, sticky=customtkinter.NSEW, columnspan=2)
@@ -35,20 +37,22 @@ class Navbar(customtkinter.CTkFrame):
 
     def _init_widget(self):
 
-        self._navbar_frama = customtkinter.CTkFrame(self, fg_color='transparent')
-        self._navbar_frama.pack(side = tk.TOP,
+        self._navbar_frame = customtkinter.CTkFrame(self, fg_color='transparent')
+        self._navbar_frame.pack(side = tk.TOP,
             fill = tk.BOTH, 
             expand = True)
 
-        self._title_app = customtkinter.CTkLabel(self._navbar_frama, text="CONTROL DE CLIENTES", font=customtkinter.CTkFont(family='Oswald', size=18, weight="bold"))
+        self._title_app = customtkinter.CTkLabel(self._navbar_frame, text="CONTROL DE CLIENTES", font=customtkinter.CTkFont(family='Oswald', size=18, weight="bold"))
         self._title_app.grid(row=0, column=0, padx=(20,0), sticky=tk.W)
-        self._hour = customtkinter.CTkLabel(self._navbar_frama, text="", font=customtkinter.CTkFont(family='Oswald', size=18, weight="bold"))
+        self._hour = customtkinter.CTkLabel(self._navbar_frame, text="", font=customtkinter.CTkFont(family='Oswald', size=18, weight="bold"))
         self._hour.grid(row=0, column=1, padx=(0,20))
+        self._btn_sign_off = customtkinter.CTkButton(self._navbar_frame, text='', image=self._icon_sign_off, width=40, command=lambda:self.manager.sign_off())
+        self._btn_sign_off.grid(row=0, column=2, padx=(0,10))
 
         # settings frame
-        self._navbar_frama.columnconfigure(0, weight=1)
-        self._navbar_frama.rowconfigure(0, weight=1)
-        self._navbar_frama.rowconfigure(0, weight=1)        
+        self._navbar_frame.columnconfigure(0, weight=1)
+        self._navbar_frame.rowconfigure(0, weight=1)
+        self._navbar_frame.rowconfigure(0, weight=1)        
 
 
     def _finish_execution(self, e):
