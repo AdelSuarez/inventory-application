06e0d59e91bcec96c4c311dff8801d4083aa8aa8
adel suarez <adel90suarez@gmail.com>
2023-03-04 21:39:10 -0400

    The window settings were changed and the function that performs the frame change was created.

--- Manager.py
+++ Manager.py
@@ -2,6 +2,7 @@ import customtkinter
 from components.NavBar import Navbar
 from components.NotificationBar import NotificationBar
 from screens.Home import HomeScreen
+from screens.Login import LoginScreen
 customtkinter.set_appearance_mode("System")
 
 class Manager(customtkinter.CTk):
@@ -9,12 +10,31 @@ class Manager(customtkinter.CTk):
         super().__init__()
         self.container = customtkinter.CTkFrame(self)
         self._configure_window()
-        self._configure_self()
         self.container.pack(expand=True, fill=customtkinter.BOTH)
-        self._window()
 
-    def _window(self):
+        self.frames = {}
+        screens = (LoginScreen, HomeScreen)
+        for f in screens:
+            frame = f(self.container, self)
+            self.frames[f] = frame
+            frame.grid(row=0, column=0, sticky=customtkinter.NSEW)
+        
+        self.show_frame(LoginScreen)
+
+    def show_frame(self, container):
+        frame = self.frames[container]
+        frame.tkraise()
+
+    def login_to_home(self):
+        self.show_frame(HomeScreen)
+    
+    def sign_off(self):
+        self.show_frame(LoginScreen)
+
+
+    def _home_screen(self):
         # main window widget structure
+        self._configure_home_screen()
         self.navbar = Navbar(master=self.container)
         self.Home = HomeScreen(master=self.container)
         self.notification_bar = NotificationBar(master=self.container )
@@ -24,11 +44,5 @@ class Manager(customtkinter.CTk):
         self.geometry('1230x735')
         self.minsize(width=1230, height=735)
         self.title('Control de clientes')
-
-
-    def _configure_self(self):
-        # Configuration of the upper menu, so that it is expandible
         self.container.columnconfigure(0, weight=1)
-        self.container.rowconfigure(0, minsize=70)
-        self.container.rowconfigure(1, weight=1,)
-        self.container.rowconfigure(2, minsize=20)
+        self.container.rowconfigure(0, weight=1)
